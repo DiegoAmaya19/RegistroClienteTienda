59d8836c6ca67960ead49661ec63fc5c045f6e64
DiegoAmaya19 <141657295+DiegoAmaya19@users.noreply.github.com>
2025-10-08 09:31:42 -0500

    ver

--- BackendRegistroClienteTienda/tienda/src/main/java/com/registro/tienda/Repository/ClienteRepository.java
+++ BackendRegistroClienteTienda/tienda/src/main/java/com/registro/tienda/Repository/ClienteRepository.java
@@ -2,7 +2,6 @@ package com.registro.tienda.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import com.registro.tienda.Model.Document.Cliente;
 
 @Repository
